0541bb9f53e68ee8b01a734732c0e9ec9a6f7d69
david.ftzi@gmail.com <david.ftzi@gmail.com@b63fe3a8-c3b5-dc40-ee86-ae054c6bfa47>
2012-11-24 18:46:27 +0000

    base application

---
+++ trunk/my-nfc-friend/src/com/nfcfriend/MainActivity.java
@@ -0,0 +1,79 @@
+package com.nfcfriend;
+
+
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.NfcAdapter.CreateNdefMessageCallback;
+import android.nfc.NfcAdapter.OnNdefPushCompleteCallback;
+import android.nfc.NfcEvent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Parcelable;
+import android.util.Log;
+import android.widget.Toast;
+import android.app.Activity;
+import android.content.Intent;
+
+public class MainActivity extends Activity implements CreateNdefMessageCallback, OnNdefPushCompleteCallback {
+
+	public static final int MESSAGE_SENT = 1;
+	private NfcAdapter nfcAdapter;
+	private NdefMessage ndefMessage;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+		if(nfcAdapter == null) {
+			Log.e("ERROR", "NFC is not available");
+		} else {
+			nfcAdapter.setNdefPushMessageCallback(this, this);
+			nfcAdapter.setOnNdefPushCompleteCallback(this, this);
+		}
+	}
+
+	private Handler nfcHandler = new Handler() {
+		public void handleMessage(final Message msg) {
+			switch (msg.what) {
+			case MESSAGE_SENT:
+				Toast.makeText(getApplicationContext(), "Message sent!", Toast.LENGTH_SHORT).show();
+				break;
+			default:
+				break;
+			}
+		};
+	};
+	
+	@Override
+	public NdefMessage createNdefMessage(final NfcEvent event) {
+		ndefMessage = new NdefMessage(NdefRecord.createMime("application/com.nfcfriend", "David phone".getBytes()));
+		return ndefMessage;
+	}
+
+	@Override
+	public void onNdefPushComplete(final NfcEvent event) {
+		nfcHandler.obtainMessage(MESSAGE_SENT).sendToTarget();
+	}
+	
+	private void processIntent(final Intent intent) {
+		Parcelable[] rawMessage = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+		NdefMessage msg = (NdefMessage) rawMessage[0];
+		Log.i("NFCFriend", new String(msg.getRecords()[0].getPayload()));
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
+			processIntent(getIntent());
+		}
+ 	}
+	
+	@Override
+	protected void onNewIntent(final Intent intent) {
+		setIntent(intent);
+	}
+}
